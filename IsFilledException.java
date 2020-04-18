58d618fcceebe6969f0364d5641c55e93ec8d062
hollyyph <hollyana.puteri.haryono@gmail.com>
2020-04-19 06:48:22 +0700

    update exception

--- IsFilledException.java
+++ IsFilledException.java
@@ -1,9 +1,24 @@
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.Timer;
 
 public class IsFilledException extends Exception{
-    new JFrame(){
+    IsFilledException(){
         
+        JFrame f = new JFrame();
+        JPanel p = new JPanel();
+        JLabel message = new JLabel("Sudah Terisi", JLabel.CENTER);
+        p.add(message);
+        f.add(p);
+        f.setSize(200, 80);
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        // set timer
+    }
+
+    public static void main (String args[]){
+        new IsFilledException();
     }
     
 }
--- LoseException.java
+++ LoseException.java
@@ -1,7 +1,17 @@
+import javax.swing.JLabel;
+import javax.swing.*;
 
 public class LoseException extends Exception{
-    new JFrame{
-        JPanel attention = new JPanel() 
-        
+    LoseException(){
+        JFrame f = new JFrame();
+        JPanel p = new JPanel();
+        JLabel message = new JLabel("You LOSE", JLabel.CENTER);
+        p.add(message);
+        f.add(p);
+        f.setSize(200, 80);
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
     }
 }
+
